d7376fc4077427b5971ba535a3f3124362a312da
eagle-eyed <foya@ukr.net>
2016-02-03 11:08:18 +0200

    Chapter 2 - Adding ClockWidgets to the TimeZoneView
    This change adds a list of ClockWidgets to the TimeZoneView, so that
    each tab display clocks in the time zones for that region.

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneView.java
@@ -18,9 +18,12 @@ import java.util.TimeZone;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
+import com.packtpub.e4.clock.ui.ClockWidget;
 import com.packtpub.e4.clock.ui.internal.TimeZoneComparator;
 
 public class TimeZoneView extends ViewPart {
@@ -41,6 +44,18 @@ public class TimeZoneView extends ViewPart {
 					regionIterator.next();
 			CTabItem item = new CTabItem(tabs, SWT.NONE);
 			item.setText(region.getKey());
+			Composite clocks = new Composite(tabs, SWT.NONE);
+			item.setControl(clocks);
+			clocks.setLayout (new RowLayout());
+			RGB rgb = new RGB(128, 128, 128);
+			TimeZone td = TimeZone.getDefault();
+			Iterator<TimeZone> timezoneIterator = region.getValue().iterator();
+			while (timezoneIterator.hasNext()) {
+				TimeZone tz = timezoneIterator.next();
+				ClockWidget clock = new ClockWidget(clocks, SWT.NONE, rgb);
+				clock.setOffset((tz.getOffset(System.currentTimeMillis()) - td
+						.getOffset(System.currentTimeMillis())) / 3600_000);
+			}
 		}
 		tabs.setSelection(0);
 	}
